1ae363e55c925179bac36835c28a7f4a4a6b4367
Juliette Bourdiau <juliette.bourdiau@gmail.com>
2024-10-01 13:22:03 +0200

    double jump

--- Assets/Scripts/PlayerJumps.cs
+++ Assets/Scripts/PlayerJumps.cs
@@ -9,32 +9,53 @@ public class PlayerJumps : MonoBehaviour
     [SerializeField] Vector2 jumpImpulse;
     [SerializeField] GameObject isJumping;
     float distance = 0.1f;
-    [SerializeField] float jumpTimer = 0;
+    [SerializeField] int maxJumps;
+    int jumpsLeft;
     bool canDoubleJump = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumpsLeft = maxJumps;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsGrounded())
+        {
+            canDoubleJump = false;
+            jumpsLeft = maxJumps;
+        }
+        if (!IsGrounded())
+        {
+            canDoubleJump = true;
+        }
     }
     public void OnJump() {
+        if (IsGrounded())
+        {
+            rb.AddForce(jumpImpulse, ForceMode2D.Impulse);
+            jumpsLeft--;
+        }
+        else if (canDoubleJump && jumpsLeft > 0)
+        {
+            rb.AddForce(1.5f * jumpImpulse, ForceMode2D.Impulse);
+            jumpsLeft--;
+        }
+    }
+
+    bool IsGrounded()
+    {
         RaycastHit2D hit = Physics2D.Raycast(isJumping.transform.position, Vector2.down, distance);
         if (hit.collider != null)
         {
-            rb.AddForce(jumpImpulse);
-            jumpTimer = Time.time + 2;
-            canDoubleJump = true;
-            //Debug.Log("jump!");
+            Debug.Log("is grounded");
+            return true;
         }
-        if (canDoubleJump && Time.time <= jumpTimer)
+        else
         {
-            rb.AddForce(jumpImpulse);
-            canDoubleJump = false;
-            //Debug.Log("Double jump!");
+            Debug.Log("is not grounded");
+            return false;
         }
     }
 }
